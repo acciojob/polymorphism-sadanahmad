1187112da03a9b3ca044b887efb4b9991e603b67
unknown <sadanahmad96@gmail.com>
2023-01-13 00:50:43 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,23 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is my solution");
+        Product p = new Product();
+        System.out.println(p.product(5,10));
+
     }
+
+    static class Product{
+    public int product(int x, int y) {
+        return  x + y;
+    }
+
+    public int product(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    public double product(double x, double y) {
+        return  x + y;
+    }
+}
+
 }
